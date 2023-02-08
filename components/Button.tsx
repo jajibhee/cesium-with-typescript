0b73510213766c14a079f4e8f65b4ec3af98b381
jaji oluwatobiloba <jajmantobi@gmail.com>
2023-02-07 22:22:03 -0500

    Add button component

---
+++ components/Button.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+type ButtonProps = {
+  route?: string;
+  color?: string;
+  name: string;
+};
+const Button: React.FC<ButtonProps> = ({ route, name, color = "bg-red" }) => {
+  return (
+    <button className="hover:bg-blue-400 group flex items-center rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm">
+      <svg
+        width="20"
+        height="20"
+        fill="currentColor"
+        className="mr-2"
+        aria-hidden="true"
+      >
+        <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
+      </svg>
+      {name}
+    </button>
+  );
+};
+
+export default Button;
